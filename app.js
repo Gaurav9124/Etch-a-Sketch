735c815b0cb59423be0f2e6990405b8f40b81bab
Gaurav Raut <zgaurav713@gmail.com>
2024-11-03 11:26:59 +0530

    incomplete

--- app.js
+++ app.js
@@ -1,39 +1,26 @@
 console.log("hello");
+const dimension = document.getElementById("dimension");
+const color = document.getElementById("color");
 const container = document.querySelector(".container");
-const grid_dimensions = document.querySelector(".grid-dimensions");
-const grid_button = document.querySelector("#button");
-const eraser = document.getElementById("eraser");
-const reset = document.getElementById("reset");
-let gridDimensions = 16;
-var pixelSize = parseInt(600 / gridDimensions);
-var totalPixels = parseInt(gridDimensions * gridDimensions);
+const dimen=document.getElementById('dimen')
 
-grid_button.addEventListener("click", () => {
-  gridDimensions = parseInt(grid_dimensions.value);
-  pixelSize = parseInt(600 / gridDimensions);
-  totalPixels = parseInt(gridDimensions * gridDimensions);
-
-  container.innerHTML = "";
-
-  for (let i = 0; i < totalPixels; i++) {
-    const pixel = document.createElement("div");
-    pixel.className = "pixel-class";
-    pixel.id = "pixel-id";
-    pixel.style.height = `${pixelSize}px`;
-    pixel.style.width = `${pixelSize}px`;
-    pixel.style.border = "0px solid black";
-    container.appendChild(pixel);
-
-    pixel.addEventListener("mouseover", () => {
-      pixel.style.backgroundColor = "purple";
-      pixel.style.cursor = "pointer";
-    });
-    container.append(pixel);
-  }
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed.");
+  console.log(dimension.ariaValueMax)
+  // Your code to execute when DOM is ready
 });
 
-console.log(totalPixels);
-
-// reset.addEventListener("click", () => {
-//   container.removeAttribute("stye");
-// });
+let i = 0;
+let j = 0;
+for (i; i < 16; i++) {
+  for (j=0; j < 16; j++) {
+    let cell = document.createElement("div");
+    cell.className = "cell-class";
+    cell.id = "cell-id";
+    cell.style.height = "60px";
+    cell.style.width = "60px";
+    cell.style.border = "1px solid black";
+    cell.style.margin='0px';
+    container.appendChild(cell);
+  }
+}
